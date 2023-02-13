0c8255ce9646e51d7264a506d967f6af66869642
zhenya30000 <eugeny.sverdlov@gmail.com>
2023-02-13 22:39:37 +0400

    refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format <type>', 'Output format', 'tree')
+  .option('-f, --format <type>', 'Output format', 'stylish')
   .action((firstConfig, secondConfig, formatName) => {
     console.log(gendiff(firstConfig, secondConfig, formatName.format));
   });
