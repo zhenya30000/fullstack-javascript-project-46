9962c859d351f7fd477e8e0f737fee37b417d0e1
zhenya30000 <eugeny.sverdlov@gmail.com>
2023-02-20 13:20:32 +0400

    Tests refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,54 +1,25 @@
 import fs from 'fs';
 import gendiff from '../src/index.js';
 
-test('Recursive mainflow with JSON files', () => {
+const stylish = fs.readFileSync('./__fixtures__/stylish', 'utf-8');
+const plain = fs.readFileSync('./__fixtures__/plain', 'utf-8');
+const json = fs.readFileSync('./__fixtures__/json', 'utf-8');
+const extensions = ['yaml', 'yml', 'json'];
+
+test.each(extensions)('Stylish output with %s configs', (ext) => {
   expect(
-    gendiff('./__fixtures__/recursiveFile1.json', './__fixtures__/recursiveFile2.json'),
-  ).toEqual(fs.readFileSync('./__fixtures__/recursiveDiff', 'utf-8'));
+    gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`),
+  ).toEqual(stylish);
 });
 
-test('Recursive mainflow with YML files', () => {
+test.each(extensions)('Plain output with %s configs', (ext) => {
   expect(
-    gendiff('./__fixtures__/recursiveYaml.yml', './__fixtures__/recursiveYaml2.yml'),
-  ).toEqual(fs.readFileSync('./__fixtures__/recursiveDiff', 'utf-8'));
+    gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`, 'plain'),
+  ).toEqual(plain);
 });
 
-test('Plain output', () => {
+test.each(extensions)('JSON output with %s configs', (ext) => {
   expect(
-    gendiff('./__fixtures__/recursiveFile1.json', './__fixtures__/recursiveFile2.json', 'plain'),
-  ).toEqual(fs.readFileSync('./__fixtures__/plainDiff', 'utf-8'));
-});
-
-test('JSON output', () => {
-  expect(
-    gendiff('./__fixtures__/recursiveFile1.json', './__fixtures__/recursiveFile2.json', 'json'),
-  ).toEqual(fs.readFileSync('./__fixtures__/jsonDiff', 'utf-8'));
-});
-
-test('Function mainflow with JSON files', () => {
-  expect(
-    gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json'),
-  ).toEqual(
-    '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}',
-  );
-});
-
-test('Function mainflow with YML files', () => {
-  expect(
-    gendiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml'),
-  ).toEqual(
-    '{\n  + mode: hexlet\n    name: github-actions\n    on: push\n  - test: true\n  + test: false\n}',
-  );
-});
-
-test('Passes with empty files', () => {
-  expect(
-    gendiff('./__fixtures__/file3.json', './__fixtures__/file3.json'),
-  ).toEqual('{\n\n}');
-});
-
-test('Passes with keys equals values', () => {
-  expect(
-    gendiff('./__fixtures__/file4.json', './__fixtures__/file3.json'),
-  ).toEqual('{\n  - host: host\n  - timeout: timeout\n  - verbose: verbose\n}');
+    gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`, 'json'),
+  ).toEqual(json);
 });
