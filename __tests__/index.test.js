e75724dc4ebc11322e6a0c05d91505af2478d8e4
zhenya30000 <eugeny.sverdlov@gmail.com>
2023-02-20 13:30:59 +0400

    Tests refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,21 +5,16 @@ const stylish = fs.readFileSync('./__fixtures__/stylish', 'utf-8');
 const plain = fs.readFileSync('./__fixtures__/plain', 'utf-8');
 const json = fs.readFileSync('./__fixtures__/json', 'utf-8');
 const extensions = ['yaml', 'yml', 'json'];
+const getResult = (ext, type) => gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`, type);
 
 test.each(extensions)('Stylish output with %s configs', (ext) => {
-  expect(
-    gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`),
-  ).toEqual(stylish);
+  expect(getResult(ext, 'stylish')).toEqual(stylish);
 });
 
 test.each(extensions)('Plain output with %s configs', (ext) => {
-  expect(
-    gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`, 'plain'),
-  ).toEqual(plain);
+  expect(getResult(ext, 'plain')).toEqual(plain);
 });
 
 test.each(extensions)('JSON output with %s configs', (ext) => {
-  expect(
-    gendiff(`./__fixtures__/file1.${ext}`, `./__fixtures__/file2.${ext}`, 'json'),
-  ).toEqual(json);
+  expect(getResult(ext, 'json')).toEqual(json);
 });
